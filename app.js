9dd1c2145e243b35020abcdf8430b981ecc67c3b
xxghoulpr0xx <javieralvarado1102@gmail.com>
2025-01-20 15:54:18 -0600

    Juego completado

---
+++ app.js
@@ -0,0 +1,36 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombres=[]
+
+function setNombre(){
+    nombre=document.getElementById("amigo").value.trim();
+    if(nombre!== ''){
+        pushFriends(document.querySelector('#amigo').value)
+        document.querySelector('#amigo').value = '';
+    }
+    else{
+        alert("no valido")
+    }
+}
+
+function pushFriends(nombre){
+    nombres.push(nombre)
+    document.getElementById('listaAmigos').innerHTML = '';
+    for (let i = 0; i <= nombres.length; i++) {
+        const nuevoElemento = document.createElement('li');
+        nuevoElemento.textContent = nombres[i];
+        document.getElementById('listaAmigos').appendChild(nuevoElemento);
+    }
+}
+
+function getNombre(){
+    if(nombres.length!== 0){
+        document.getElementById('resultado').innerHTML="";
+        let aleatorio = Math.floor(Math.random()*(nombres.length-1))+1;
+        const nuevoElemento = document.createElement('li');
+        nuevoElemento.textContent = nombres[aleatorio];
+        document.getElementById('resultado').appendChild(nuevoElemento);
+    }
+    else{
+        alert("La lista esta vacia");
+    }
+}
